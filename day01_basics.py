39832470f7bc15387a5f23f546e57e30c3f54848
rics254 <42621460+rics254@users.noreply.github.com>
2025-07-30 10:57:52 +0300

    Add Day 1 – Print, Input, Variables

---
+++ day01_basics.py
@@ -0,0 +1,9 @@
+# Day 1 – Print, Input, Variables
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print(f"Hello {name}, you will be {age + 5} in 5 years time.")
+
+city = input("What city do you live in? ")
+print(f"I have never been to {city} before.")
